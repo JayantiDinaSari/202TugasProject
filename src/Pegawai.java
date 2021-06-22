4f1f2721090423e63f52c269f15fa929e2508ee5
JayantiDinaSari <xapkb15jayanti@gmail.com>
2021-06-22 12:21:10 +0800

    membuatclass dengan 8 atribut dan 4 method

--- src/Pegawai.java
+++ src/Pegawai.java
@@ -1,5 +1,26 @@
+import java.util.Date;
+
 public class Pegawai {
     private int id;
     private String nik;
+    private String nama;
+    private Date tanggalLahir;
+    private String alamat;
+    private String jabatan;
+    private String golongan;
+    private String status;
+
+    public void bertugas(){
+        System.out.println("Sedang Bertugas");
+    }
+    public void sedangCuti(){
+        System.out.println("Sedang Cuti");
+    }
+    public void mangajukanKenaikanJabatan(){
+
+    }
+    public void mengajukanKenaikanGaji(){
+
+    }
 
 }
