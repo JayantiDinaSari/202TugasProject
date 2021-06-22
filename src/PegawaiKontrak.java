8925365d2a078a57e2456b48d0eba52237c4acfc
JayantiDinaSari <xapkb15jayanti@gmail.com>
2021-06-22 12:27:41 +0800

    membuat sub class dengan 3 atribut dan 2 method

---
+++ src/PegawaiKontrak.java
@@ -0,0 +1,12 @@
+public class PegawaiKontrak {
+    private String nikk;
+    private String asalInstansi;
+    private int lamaKontrak;
+
+    public void penambahanKontrak(){
+
+    }
+    public void pemutusanKontrak(){
+
+    }
+}
